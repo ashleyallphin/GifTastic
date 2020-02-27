0ee34928e76c6f63e4b7373f665d2afa4d7528cd
Ashley Allphin <ashley.allphin@gmail.com>
2020-02-26 22:40:10 -0500

    receiving data successfully

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,14 +1,12 @@
-var buttonArray = [
-    "dog", "baseball", "Garth Brooks", "The University of Texas", "Noel Fielding", "The Mighty Boosh", "Kurupt FM", "Maria Sharapova", "Atlanta Braves", "The Office", "annoyed", "Pantone", "FRIENDS", "London", "unicorn", "dinosaur", "clouds"
+/*
+    //make an array of keywords
+    var buttonArray = [
+    "dog", "baseball", "Garth Brooks", "Texas Longhorns", "Noel Fielding", "The Mighty Boosh", "GBBO" "Kurupt FM", "Maria Sharapova", "Atlanta Braves", "The Office", "annoyed", "Pantone", "FRIENDS", "London", "unicorn", "dinosaur", "clouds"
 ]
 
+========
 
-
-
-
-
-//========
-
+//push keywords to page
 for (var i=0; i<buttonArray.length; i++) {
     console.log(buttonArray[i]);
     $('#keyword-buttons').append(
@@ -18,3 +16,61 @@ for (var i=0; i<buttonArray.length; i++) {
 $('button').on("click", function() {
     console.log("button-clicked");
 })
+
+*/
+/*
+
+$('button').on('click', function() {
+  
+    //grab the arrays by our category tags
+    var data = $(this).data("category");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=0&lang=en";
+    $.ajax({url:queryURL,method:"GET"})
+        .done(function(response){
+            console.log(response);
+        })
+    
+        for (var i=0; i<response.data.length; i++) {
+            console.log(response.length);
+            $("#response").append('scr',response.data[i].images.fixed_height_still.url);
+        }
+
+})
+
+*/
+
+
+
+
+
+
+// $('button').on('click', function() {
+//     var data = $(this).data("category");
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=0&lang=en";
+    
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       }).then(function(response) {
+//         console.log(response);
+//       ))}
+
+
+
+
+$('button').on("click", function() {
+    var data = $(this).data("category");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=0&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        }).then(function(response) {
+            for (var i=0; i<response.data.length; i++) {
+                console.log(response.data[i].images.fixed_height_still.url);
+            }
+        })
+})
+
+
+
