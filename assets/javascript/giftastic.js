fd73def7d48c88d459c9545cfb19de730ea0270a
Ashley Allphin <ashley.allphin@gmail.com>
2020-02-27 23:55:48 -0500

    buttons are added dynamically now

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,64 +1,50 @@
-/*
+//===========================================
+//VARIABLES
+    
+    
     //make an array of keywords
     var buttonArray = [
-    "dog", "baseball", "Garth Brooks", "Texas Longhorns", "Noel Fielding", "The Mighty Boosh", "GBBO" "Kurupt FM", "Maria Sharapova", "Atlanta Braves", "The Office", "annoyed", "Pantone", "FRIENDS", "London", "unicorn", "dinosaur", "clouds"
-]
+    "dog", "baseball", "Garth Brooks", "Texas Longhorns", "Noel Fielding", "The Mighty Boosh", "GBBO", "Kurupt FM", "Maria Sharapova", "Atlanta Braves", "The Office", "annoyed", "Pantone", "FRIENDS", "London", "unicorn", "dinosaur", "clouds"
+];
 
-========
+//===========================================
+//FUNCTIONS
 
-//push keywords to page
-for (var i=0; i<buttonArray.length; i++) {
-    console.log(buttonArray[i]);
-    $('#keyword-buttons').append(
-        '<button class="btn">' + buttonArray[i] + '</button>');
-}
+//function for putting the array buttons on the page
 
-$('button').on("click", function() {
-    console.log("button-clicked");
-})
+function renderButtons() {
 
-*/
-/*
+// Deleting the movie buttons prior to adding new movie buttons
+        // (this is necessary, or we would have repeat buttons)
+        $("#GIFimages").empty();
 
-$('button').on('click', function() {
-  
-    //grab the arrays by our category tags
-    var data = $(this).data("category");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=0&lang=en";
-    $.ajax({url:queryURL,method:"GET"})
-        .done(function(response){
-            console.log(response);
-        })
-    
-        for (var i=0; i<response.data.length; i++) {
-            console.log(response.length);
-            $("#response").append('scr',response.data[i].images.fixed_height_still.url);
-        }
+        //loop through array
+    for (var i=0; i<buttonArray.length; i++) {
+        //console.log(buttonArray[i]);
 
-})
+          // create a button for each category in the array.
+          var a = $("<button>");
+          // Adding a class
+          a.addClass("category");
+          a.addClass("btn");
+          // Adding a data-attribute with a value of the movie at index i
+          a.attr("data-category", buttonArray[i]);
+          // Providing the button's text with a value of the movie at index i
+          a.html(buttonArray[i]);
+          // Adding the button to the HTML
+          $(".keyword-buttons").append(a);
 
-*/
+          //console.log(a);
 
+}}
 
-
-
-
-
-// $('button').on('click', function() {
-//     var data = $(this).data("category");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=0&lang=en";
-    
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       }).then(function(response) {
-//         console.log(response);
-//       ))}
-
-
+renderButtons();
 
 
 $('button').on("click", function() {
+    
+    $("#GIFimages").empty();
+
     var data = $(this).data("category");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=10&lang=en";
 
@@ -80,13 +66,8 @@ $('button').on("click", function() {
                 //prepend in here:
                 $("#GIFimages").prepend(result);
 
-                $("#GIFimages").prepend('<img id="GIFimage" src=' + response.data[i].images.fixed_height_still.url + '/> ');
+                
             }
         })
 })
-
-
-
-//on click, animate
-
 
